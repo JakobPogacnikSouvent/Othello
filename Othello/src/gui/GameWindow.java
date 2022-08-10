ef2e11c156700beca6e11cd145a00eac5c707682
Jakob Pogacnik Souvent <jakob.pogacnik.s@gmail.com>
2022-08-10 20:28:36 +0200

    Add files via upload

--- Othello/src/gui/GameWindow.java
+++ Othello/src/gui/GameWindow.java
@@ -4,55 +4,39 @@ import javax.swing.*;
 
 import inteligenca.Inteligenca;
 import logic.Game;
+import logic.GameStatus;
 import logic.Stone;
 
-import java.awt.Color;
 import java.awt.event.*;
 
 @SuppressWarnings("serial")
 public class GameWindow extends JFrame implements ActionListener {
 	
-	protected Platno platno;
-
-	private JLabel status;
-	private JLabel rezPrimary, rezSecondary;
+	private JLabel rez;
 	
-	private JMenuItem menuBarvaIgralecEna, menuBarvaIgralecDve, menuBarvaPolja;
+	protected Platno platno;
+	private JMenuItem menuOdpri, menuShrani, menuKoncaj;
+	private JMenuItem menuPrazen, menuCikel, menuPoln, menuPolnDvodelen;
+	private JMenuItem menuBarvaPovezave, menuBarvaTocke, menuBarvaAktivneTocke, menuBarvaIzbraneTocke, menuBarvaRoba;
+	private JMenuItem menuDebelinaRoba, menuDebelinaPovezave;
+	
+	Timer timer;
+	
+	public void changeScore(int scoreWhite, int scoreBlack) {
+		rez.setText(scoreWhite + " || " + scoreBlack);
+	}
 	
 	public GameWindow(Inteligenca ai, Stone aiColour) {
 		super();
-		setTitle("Othello 2");
+		setTitle("Othello 1.2");
 		
-		// Ureditev orodne vrstice
-		JMenuBar menubar = new JMenuBar();
-		setJMenuBar(menubar);
-		
-		JMenu menuNastavitve = dodajMenu(menubar, "Nastavitve");
-		
-		menuBarvaIgralecEna = dodajItem(menuNastavitve, "Nastavi barvo prvega igralca.");
-		menuBarvaIgralecDve = dodajItem(menuNastavitve, "Nastavi barvo drugega igralca.");
-		menuBarvaPolja = dodajItem(menuNastavitve, "Nastavi barvo polj.");
-		
-		// Glavna ploÅ¡Ä�a
+		// Glavna plošča
 		JPanel glavnaPlosca = new JPanel();
 		glavnaPlosca.setLayout(new BoxLayout(glavnaPlosca, BoxLayout.Y_AXIS));
 		this.add(glavnaPlosca);
 		
-		//===========================================================================
-		
-		// JPanel, ki je nad igralnim poljem
-		JPanel strop = new JPanel();
-		
-		// Sporočilo na stropu
-		status = new JLabel();
-		strop.add(status);
-		
-		glavnaPlosca.add(strop);
-		
-		//===========================================================================
-		
 		// Igralno polje
-		platno = new Platno(600, 600);
+		platno = new Platno(800, 800, this);
 		glavnaPlosca.add(platno);
 		
 		if (ai != null) {
@@ -61,30 +45,45 @@ public class GameWindow extends JFrame implements ActionListener {
 			platno.setGame(new Game());
 		}
 		
-		//===========================================================================
-		
 		// JPanel, ki je pod igralnim poljem
 		JPanel temelj = new JPanel();
 		
 		// Števec točk
-		rezPrimary = new JLabel();
-		rezPrimary.setBackground(platno.squareColourPrimary);
-		rezPrimary.setForeground(Color.BLACK);
-		rezPrimary.setOpaque(true);
-		rezSecondary = new JLabel();
-		rezSecondary.setBackground(platno.squareColourSecondary);
-		rezSecondary.setForeground(Color.WHITE);
-		rezSecondary.setOpaque(true);
-		temelj.add(rezPrimary);
-		temelj.add(rezSecondary);
+		rez = new JLabel();
+		temelj.add(rez);
 		
 		glavnaPlosca.add(temelj);
 		
-		status.setText("Pozdravljeni!");
-		rezPrimary.setText(String.valueOf(platno.game.blackScore));
-		rezSecondary.setText(String.valueOf(platno.game.whiteScore));
-		
 		platno.repaint();
+		
+		timer = new Timer(1000, this);
+		timer.start();
+		
+		update();
+		
+//		JMenuBar menubar = new JMenuBar();
+//		setJMenuBar(menubar);
+//		
+//		JMenu menuDatoteka = dodajMenu(menubar, "Datoteka");
+//		JMenu menuGraf = dodajMenu(menubar, "Graf");
+//		JMenu menuNastavitve = dodajMenu(menubar, "Nastavitve");
+//		
+//		menuOdpri = dodajMenuItem(menuDatoteka, "Odpri ...");
+//		menuShrani = dodajMenuItem(menuDatoteka, "Shrani ...");
+//		menuKoncaj = dodajMenuItem(menuDatoteka, "Končaj");
+//		menuPrazen = dodajMenuItem(menuGraf, "Prazen ...");
+//		menuCikel = dodajMenuItem(menuGraf, "Cikel ...");
+//		menuPoln = dodajMenuItem(menuGraf, "Poln ...");
+//		menuPolnDvodelen = dodajMenuItem(menuGraf, "Poln dvodelen ...");
+//		menuBarvaPovezave = dodajMenuItem(menuNastavitve, "Barva povezave ...");
+//		menuBarvaTocke = dodajMenuItem(menuNastavitve, "Barva točke ...");
+//		menuBarvaAktivneTocke = dodajMenuItem(menuNastavitve, "Barva aktivne točke ...");
+//		menuBarvaIzbraneTocke = dodajMenuItem(menuNastavitve, "Barva izbrane točke ...");
+//		menuBarvaRoba = dodajMenuItem(menuNastavitve, "Barva roba ...");
+//		menuDebelinaRoba = dodajMenuItem(menuNastavitve, "Debelina roba ...");
+//		menuDebelinaPovezave = dodajMenuItem(menuNastavitve, "Debelina povezave ...");
+//		
+//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	private JMenu dodajMenu(JMenuBar menubar, String naslov) {
@@ -93,40 +92,112 @@ public class GameWindow extends JFrame implements ActionListener {
 		return menu;
 	}
 
-	private JMenuItem dodajItem(JMenu menu, String naslov) {
+	private JMenuItem dodajMenuItem(JMenu menu, String naslov) {
 		JMenuItem menuItem = new JMenuItem(naslov);
 		menu.add(menuItem);
 		menuItem.addActionListener(this);
 		return menuItem;		
 	}
 	
-	public static void updateMessages() {
+	public void update() {
+		platno.repaint();
+		int whiteScore = platno.getGame().getWhiteScore();
+		int blackScore = platno.getGame().getBlackScore();
 		
+		changeScore(whiteScore, blackScore);
+	
+		if (platno.getGame().getStatus() == GameStatus.FINISHED) {
+	    	timer.stop();
+			
+			String[] options = new String[] {"Play again!", "Main menu!", "Exit"};
+		    
+			String winningText = "";
+			switch (platno.getGame().getWinner()) {
+			case WHITE:
+				winningText = "Final score is: " + whiteScore + " to " + blackScore + "\nWHITE wins!";
+				break;
+			case BLACK:
+				winningText = "Final score is: " + whiteScore + " to " + blackScore + "\nBLACK wins!";
+				break;
+			case EMPTY:
+				winningText = "Final score is: " + whiteScore + " to " + blackScore + "\nGame ended in a TIE!";
+				break;
+			}
+			
+			int playerChoice = JOptionPane.showOptionDialog(null, winningText, "Game finished", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+		      
+		    switch (playerChoice) {
+		    case 0 :
+		    	platno.getGame().reset();
+		    	timer.restart();
+		    	update();
+		    	break;
+		    case 1:
+		    	dispose();
+		    	break;
+		    case 2:
+		    	System.exit(0);
+		    	break;
+		    }
+		}
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		Object source = e.getSource();
-		if (source == menuBarvaIgralecEna) {
-			Color izbira = JColorChooser.showDialog(this, "Barva kamenčkov prvega igralca", platno.figureColourBlack);
-			if (izbira == null) return;
-			platno.figureColourBlack = izbira;
-			platno.repaint();
+		// TODO Auto-generated method stub
+		if (e.getSource() == timer) {
+			update();
 		}
-		else if (source == menuBarvaIgralecDve) {
-			Color izbira = JColorChooser.showDialog(this, "Barva kamenčkov drugega igralca", platno.figureColourWhite);
-			if (izbira == null) return;
-			platno.figureColourWhite = izbira;
-			platno.repaint();
-		}
-		else if (source == menuBarvaPolja) {
-			Color izbira = JColorChooser.showDialog(this, "Barva igralnih polj", platno.squareColourPrimary);
-			if (izbira == null) return;
-			platno.squareColourPrimary = izbira;
-			Color darker = izbira.darker();
-			platno.squareColourSecondary = darker;
-			platno.repaint();
-		}
+		// TODO Auto-generated method stub
+//		Object source = e.getSource();
+//		if (source == menuOdpri) {
+//			JFileChooser dialog = new JFileChooser();
+//			int izbira = dialog.showOpenDialog(this);
+//			if (izbira == JFileChooser.APPROVE_OPTION) {
+//				String ime = dialog.getSelectedFile().getPath();
+//				Graf graf = Graf.preberi(ime);
+//				platno.nastaviGraf(graf);
+//			}
+//		} else if (source == menuShrani) {
+//			JFileChooser dialog = new JFileChooser();
+//			int izbira = dialog.showSaveDialog(this);
+//			if (izbira == JFileChooser.APPROVE_OPTION) {
+//				String ime = dialog.getSelectedFile().getPath();
+//				platno.graf.shrani(ime);
+//			}
+//		} else if (source == menuKoncaj) {
+//			dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+//		} else if (source == menuPrazen) {
+//			String steviloTock = JOptionPane.showInputDialog(this, "Število točk:");
+//			if (steviloTock != null && steviloTock.matches("\\d+")) {
+//				Graf g = Graf.prazen(Integer.parseInt(steviloTock));
+//				g.razporedi(400, 400, 300);
+//				platno.nastaviGraf(g);
+//			}
+//		} else if (source == menuCikel) {
+//		} else if (source == menuPoln) {
+//		} else if (source == menuPolnDvodelen) {
+//			JTextField nn = new JTextField();
+//			JTextField mm = new JTextField();
+//			JComponent[] polja = {
+//				new JLabel("Vnesi N:"), nn,	
+//				new JLabel("Vnesi M:"), mm	
+//			};
+//			int izbira = JOptionPane.showConfirmDialog(this, polja, "Input", JOptionPane.OK_CANCEL_OPTION);
+//			if (izbira == JOptionPane.OK_OPTION && nn.getText().matches("\\d+") && mm.getText().matches("\\d+")) {
+//				Graf g = Graf.polnDvodelen(Integer.parseInt(nn.getText()), Integer.parseInt(mm.getText()));
+//				g.razporedi(400, 400, 300);
+//				platno.nastaviGraf(g);	
+//			}
+//		} else if (source == menuBarvaPovezave) {
+//		} else if (source == menuBarvaTocke) {
+//		} else if (source == menuBarvaAktivneTocke) {
+//		} else if (source == menuBarvaIzbraneTocke) {
+//		} else if (source == menuBarvaRoba) {
+//		} else if (source == menuDebelinaRoba) {
+//		} else if (source == menuDebelinaPovezave) {
+//		}
+//		
 	}
 	
 }
--- Othello/src/gui/Platno.java
+++ Othello/src/gui/Platno.java
@@ -2,8 +2,11 @@ package gui;
 
 import javax.swing.JPanel;
 
+import inteligenca.Inteligenca;
 import logic.Coords;
 import logic.Game;
+import logic.GameStatus;
+
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -11,15 +14,14 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
 import java.awt.event.*;
-import javax.swing.Timer;
 
 
 @SuppressWarnings("serial") 
-public class Platno extends JPanel implements MouseListener, MouseMotionListener, KeyListener, ActionListener {
+public class Platno extends JPanel implements MouseListener, MouseMotionListener, KeyListener {
 	
-	public Game game;
+	private Game game;
 
-	public Color squareColourPrimary, squareColourSecondary, figureColourWhite, figureBorderWhite, figureColourBlack, figureBorderBlack;
+	private Color squareColourPrimary, squareColourSecondary, figureColourWhite, figureBorderWhite, figureColourBlack, figureBorderBlack;
 	private Stroke edgeSize;
 	
 	private Coords boardZeroVector;
@@ -27,14 +29,15 @@ public class Platno extends JPanel implements MouseListener, MouseMotionListener
 	private Coords mouseHoverSquare;
 			
 	private int fullWidth, fullHeight, boardSize, squareSize, stoneSize, offset;
+		
+	private GameWindow parent;
 	
-	Timer timer;
-
-	
-	public Platno(int sirina, int visina) {
+	public Platno(int sirina, int visina, GameWindow parent) {
 		super();
 		setPreferredSize(new Dimension(sirina, visina));
-
+		
+		this.parent = parent;
+		
 		game = null;
 		
 		squareColourPrimary = new Color(17, 59, 8);
@@ -56,13 +59,15 @@ public class Platno extends JPanel implements MouseListener, MouseMotionListener
 		addKeyListener(this);
 		setFocusable(true);
 		
-		timer = new Timer(1000, this);
-		timer.start();
 	}
 	
 	public void setGame(Game g) {
 		game = g;
 		repaint();
+	}
+	
+	public Game getGame() {
+		return game;
 	}
 	
 	public void setWindowVars() {
@@ -147,6 +152,7 @@ public class Platno extends JPanel implements MouseListener, MouseMotionListener
 			g2.setColor(Color.blue);
 			g2.fillOval(squareZeroVector.x + offset,squareZeroVector.y  + offset, stoneSize, stoneSize);
 		}
+		
 	}
 	
 	@Override
@@ -157,6 +163,14 @@ public class Platno extends JPanel implements MouseListener, MouseMotionListener
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+		if (game == null) return;
+		
+		char key = e.getKeyChar();
+		if (key == 'r' && !game.getAiThinking() && game.getStatus() == GameStatus.ONGOING) {
+			game.odigraj(Inteligenca.getRandomMove(game));
+			parent.update();
+			repaint();
+		}
 	}
 
 	@Override
@@ -197,6 +211,10 @@ public class Platno extends JPanel implements MouseListener, MouseMotionListener
 	public void mousePressed(MouseEvent e) {
 		if (!game.getAiThinking()) {			
 			game.odigraj(mouseHoverSquare); // mouseHoverSquare should be same as pressed square
+			
+			parent.update();				
+//			if (game.getStatus() == GameStatus.ONGOING) {
+//			}
 			repaint();
 		}
 	}
@@ -219,12 +237,5 @@ public class Platno extends JPanel implements MouseListener, MouseMotionListener
 		
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		if (e.getSource() == timer) {
-			repaint();
-		}
-	}
 	
 }
